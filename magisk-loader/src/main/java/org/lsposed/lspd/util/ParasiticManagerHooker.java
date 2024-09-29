85025ce7bdaafe29785742c796d3e5a6266c62e5
constant-e <FMN_TZX@outlook.com>
2024-09-29 19:04:18 +0800

    fix: fix manager for Android 14 QPR3 and above
    from https://github.com/mywalkb/LSPosed_mod/commit/da1daff

--- magisk-loader/src/main/java/org/lsposed/lspd/util/ParasiticManagerHooker.java
+++ magisk-loader/src/main/java/org/lsposed/lspd/util/ParasiticManagerHooker.java
@@ -123,7 +123,16 @@ public class ParasiticManagerHooker {
                     protected void afterHookedMethod(MethodHookParam param) {
                         var pkgInfo = getManagerPkgInfo(null);
                         if (pkgInfo != null && XposedHelpers.getObjectField(param.thisObject, "mApplicationInfo") == pkgInfo.applicationInfo) {
-                            sendBinderToManager((ClassLoader) param.getResult(), managerService.asBinder());
+                            // Forked from https://github.com/mywalkb/LSPosed_mod/commit/da1daff
+                            // Fix for Android 14 QPR3 and above
+                            var sSourceDir = pkgInfo.applicationInfo.sourceDir;
+                            var pathClassLoader = param.getResult();
+                            var pathList = XposedHelpers.getObjectField(pathClassLoader, "pathList");
+                            List<String> lstDexPath = (List<String>) XposedHelpers.callMethod(pathList, "getDexPaths");
+                            if (!lstDexPath.contains(sSourceDir)) {
+                                XposedHelpers.callMethod(pathClassLoader, "addDexPath", sSourceDir);
+                            }
+                            sendBinderToManager((ClassLoader) pathClassLoader, managerService.asBinder());
                             unhooks[0].unhook();
                         }
                     }
