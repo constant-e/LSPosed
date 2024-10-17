3fb1ce86a85666017673dcc371fed162f8438568
constant-e <FMN_TZX@outlook.com>
2024-10-17 18:59:47 +0800

    test fix for A15 again

--- magisk-loader/src/main/java/org/lsposed/lspd/util/ParasiticManagerHooker.java
+++ magisk-loader/src/main/java/org/lsposed/lspd/util/ParasiticManagerHooker.java
@@ -79,6 +79,8 @@ public class ParasiticManagerHooker {
                 }
                 HiddenApiBridge.ApplicationInfo_resourceDirs(newAppInfo, HiddenApiBridge.ApplicationInfo_resourceDirs(appInfo));
                 newAppInfo.uid = appInfo.uid;
+                // FIXME: It seems the parsed flags is incorrect (0) on A14 QPR3
+                newAppInfo.flags = newAppInfo.flags | ApplicationInfo.FLAG_HAS_CODE;
             } catch (Throwable e) {
                 Utils.logE("get manager pkginfo", e);
             }
@@ -120,13 +122,13 @@ public class ParasiticManagerHooker {
                     protected void afterHookedMethod(MethodHookParam param) {
                         var pkgInfo = getManagerPkgInfo(null);
                         if (pkgInfo != null && XposedHelpers.getObjectField(param.thisObject, "mApplicationInfo") == pkgInfo.applicationInfo) {
-                            // Forked from https://github.com/mywalkb/LSPosed_mod/commit/da1daff
-                            // Fix for Android 14 QPR3 and above
                             var sSourceDir = pkgInfo.applicationInfo.sourceDir;
                             var pathClassLoader = param.getResult();
+                            Hookers.logD("LoadedApk getClassLoader " + pathClassLoader);
                             var pathList = XposedHelpers.getObjectField(pathClassLoader, "pathList");
-                            List<String> lstDexPath = (List<String>) XposedHelpers.callMethod(pathList, "getDexPaths");
+                            List<String> lstDexPath = (List<String>)XposedHelpers.callMethod(pathList, "getDexPaths");
                             if (!lstDexPath.contains(sSourceDir)) {
+                                Utils.logW("Could not find manager apk injected in classloader");
                                 XposedHelpers.callMethod(pathClassLoader, "addDexPath", sSourceDir);
                             }
                             sendBinderToManager((ClassLoader) pathClassLoader, managerService.asBinder());
